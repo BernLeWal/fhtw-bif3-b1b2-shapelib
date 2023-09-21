acb31ef5ae2e07ce1f56e9cf107f79839a48f86b
wallisch <walliscb@technikum-wien.at>
2023-09-21 12:29:52 +0200

    .gitignore added

---
+++ _04B1B2ShapeLib/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04B1B2ShapeLib
+{
+    public class User
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+        private string _password;
+
+        public User(int id, string name, string email, string password)
+        {
+            _id = id;
+            _name = name;
+            _email = email;
+            _password = password;
+        }
+    }
+}
