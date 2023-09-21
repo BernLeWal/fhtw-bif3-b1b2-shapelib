273587be770e9f1722657c8c0e0f71c86d232214
wallisch <walliscb@technikum-wien.at>
2023-09-21 12:21:33 +0200

    initial commit

---
+++ _04B1B2ShapeLib/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04B1B2ShapeLib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
