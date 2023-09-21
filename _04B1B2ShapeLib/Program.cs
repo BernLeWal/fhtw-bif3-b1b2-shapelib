3d76737265b14dbf6a03eb566ef641ea540b86a3
wallisch <walliscb@technikum-wien.at>
2023-09-21 12:32:35 +0200

    add User class

--- _04B1B2ShapeLib/Program.cs
+++ _04B1B2ShapeLib/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            User user1 = new User(1, "Susi Sorglos", "susi@company.com", "1234");
+            Console.WriteLine( user1 );
         }
     }
 }
