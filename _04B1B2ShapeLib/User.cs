d5e31dbc3149aaedf60bbb5108457353f7d2064f
wallisch <walliscb@technikum-wien.at>
2023-09-28 12:34:06 +0200

    improved docu

--- _04B1B2ShapeLib/User.cs
+++ _04B1B2ShapeLib/User.cs
@@ -42,6 +42,7 @@ namespace _04B1B2ShapeLib
         }
 
         // add some documentation to the EventHandler
+        // add a second line with docu
         public void EventHandler(object? sender, EventArgs e)
         {
             Console.WriteLine($"User {Name} was informed by {sender} of {e}");
