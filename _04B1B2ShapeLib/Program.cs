b8ac71c73bb4a31196b1f74bea832272bb9ff1bb
wallisch <walliscb@technikum-wien.at>
2023-09-28 12:12:56 +0200

    add delegate, lambda and event demo

--- _04B1B2ShapeLib/Program.cs
+++ _04B1B2ShapeLib/Program.cs
@@ -6,7 +6,49 @@
         {
             Console.WriteLine("Hello, World!");
             User user1 = new User(1, "Susi Sorglos", "susi@company.com", "1234");
+            User user2 = new User(2, "Peter Lustig", "peter@lustig.at", "4321");
             Console.WriteLine( user1 );
+
+            var myObject = new Program();
+            myObject.MyActionMethod(1);    // call the method directly, the classical approach
+
+            // **** DELEGATES: ****
+            Action<int> myAction = myObject.MyActionMethod;     // create a delegate
+            myAction(2);                                        // call the delegate
+
+            myObject.LongLastingCalulation(3, myAction);        // use delegate as parameter
+            myObject.LongLastingCalulation(4, (i) => { Console.WriteLine($"Lambda Callback {i} may also use {myObject}"); } );
+
+            // **** EVENTS: ****
+            // define an event handler
+            myObject.MyEvent += user1.EventHandler;
+            myObject.MyEvent += user2.EventHandler;
+            myObject.LongLastingCalulation(5);
+            myObject.MyEvent -= user1.EventHandler;
+            myObject.LongLastingCalulation(6);
+
+        }
+
+        public void MyActionMethod(int i)
+        {
+            Console.WriteLine($"MyActionMethod: {i}");
+        }
+
+        public void LongLastingCalulation(int i, Action<int> callback)
+        {            
+            Thread.Sleep(3000); // do some long lasting calculation
+            callback(i);
+        }
+
+
+        // ***** EVENT-HANDLING *****
+        public event EventHandler? MyEvent;
+        public void LongLastingCalulation(int i)
+        {
+            // do some long lasting calculation
+            Thread.Sleep(3000);
+
+            MyEvent?.Invoke(this, EventArgs.Empty);
         }
     }
 }
